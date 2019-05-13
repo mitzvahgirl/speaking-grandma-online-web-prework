9eed135c015725e51c1f4dafa66741ed0970eea1
Amy Fogelman <abfnj1@gmail.com>
2019-05-13 19:35:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,8 +12,10 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-if = speak_to_grandma
+if speak_to_grandma = ""
   puts "HUH?! SPEAK UP, SONNY!"
-  
+  NO, NOT SINCE 1938!
+  'I LOVE YOU GRANDMA!'
+  'I LOVE YOU TOO PUMPKIN!'
 end
   
